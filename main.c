dfb67b218f195c2fc2e21292bbd724eeefb34271
Yuuki Arisawa <yuuki.ari@gmail.com>
2022-04-10 18:22:39 +0900

    Enable dynamic assign major number

--- main.c
+++ main.c
@@ -14,17 +14,18 @@ MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("This is a hello driver");
 MODULE_AUTHOR("Yuuki Arisawa");
 
-#define SAMPLE_MAJOR 300
 #define DEVICE_NAME "devsample"
 
 #define DEV_FILE "samplehw"
 #define CLASS_NAME "sample"
-#define SAMPLE_MINOR 0
+
+#define SAMPLE_MINOR_BASE 0
+#define SAMPLE_MINOR_COUNT 3
 
 static struct class *my_class;
 static struct device *my_device;
 
-static int g_major_num=SAMPLE_MAJOR;
+static int g_major_num;
 struct hello_driver {
   struct device_driver driver;
 };
@@ -50,12 +51,7 @@ static ssize_t sample_write(struct file *filp,const char __user *buf,size_t coun
   return 1;
 }
 
-static const struct file_operations sample_fops = {
-  .open    = sample_open,
-  .release = sample_close,
-  .read    = sample_read,
-  .write   = sample_write,
-};
+
 
 static char *sample_devnode(struct device *dev,umode_t *mode){
   int minor=MINOR(dev->devt);
@@ -69,20 +65,43 @@ static char *sample_devnode(struct device *dev,umode_t *mode){
   }
   return NULL;
 }
+static struct cdev my_cdev;
+int minor_count=0;
 
 static int hello_init(struct hello_driver *drv){
   int ret;
   int registered=0;
+  int added=0;
   int classed=0;
-  int deviced=0;
+
+  dev_t devid;
 
   printk(KERN_ALERT "hello driver loaded\n");
-  ret=register_chrdev(g_major_num,DEVICE_NAME,&sample_fops);
+  // charactor device
+  //ret=register_chrdev(g_major_num,DEVICE_NAME,&sample_fops);
+  ret=alloc_chrdev_region(&devid,SAMPLE_MINOR_BASE,SAMPLE_MINOR_COUNT,DEVICE_NAME);
+  g_major_num=MAJOR(devid);
   if(ret){
     printk("register_chrdev error %d(%pe)\n",ret,ERR_PTR(ret));
     goto error;
   }
+  printk("%s:Major number %d was assigned\n",__func__,g_major_num);
   registered=1;
+
+  static const struct file_operations sample_fops = {
+    .open    = sample_open,
+    .release = sample_close,
+    .read    = sample_read,
+    .write   = sample_write,
+  };
+  cdev_init(&my_cdev,&sample_fops);
+  ret=cdev_add(&my_cdev,devid,SAMPLE_MINOR_COUNT);
+  if(ret<0){
+    printk("cdev_add error %d(%pe)\n",ret,ERR_PTR(ret));
+    goto error;
+  }
+  added=1;
+
   my_class=class_create(THIS_MODULE,CLASS_NAME);
   if(IS_ERR(my_class)){
     printk("class_create error (%pe)\n",ERR_CAST(my_class));
@@ -92,37 +111,50 @@ static int hello_init(struct hello_driver *drv){
   //所有権を強制する
   my_class->devnode=sample_devnode;
   classed=1;
-  my_device=device_create(my_class,NULL,
-                          MKDEV(g_major_num,SAMPLE_MINOR),
-                          NULL,
-                          DEV_FILE
-                          );
-  if(IS_ERR(my_device)){
-    printk("device_create error (%pe)\n",ERR_CAST(my_device));
-    ret=PTR_ERR(my_device);
-    goto error;
+
+  for(int i=0;i<SAMPLE_MINOR_COUNT;i++){
+    my_device=device_create(my_class,NULL,
+                            MKDEV(g_major_num,SAMPLE_MINOR_BASE+i),
+                            NULL,
+                            DEV_FILE"%d",i
+                            );
+    if(IS_ERR(my_device)){
+      printk("device_create error (%pe)\n",ERR_CAST(my_device));
+      ret=PTR_ERR(my_device);
+      goto error;
+    }
+    minor_count++;
   }
-  deviced=1;
   return 0;
  error:
-  if(deviced){
-    device_destroy(my_class,MKDEV(SAMPLE_MAJOR,SAMPLE_MINOR));
+  for(int i=0;i<minor_count;i++){
+    device_destroy(my_class,MKDEV(g_major_num,SAMPLE_MINOR_BASE+i));
+    printk("%s:device_destroy\n",__func__);
   }
   if(classed){
     class_destroy(my_class);
   }
   if(registered){
-    unregister_chrdev(g_major_num,DEVICE_NAME);
+    dev_t dev=MKDEV(g_major_num,SAMPLE_MINOR_BASE);
+    cdev_del(&my_cdev);
+    unregister_chrdev_region(dev,SAMPLE_MINOR_COUNT);
   }
   return ret;
 }
 
 static void hello_exit(struct hello_driver *drv){
-  unregister_chrdev(g_major_num,DEVICE_NAME);
+
+  dev_t dev=MKDEV(g_major_num,SAMPLE_MINOR_BASE);
   printk(KERN_ALERT "hello driver unloaded\n");
-  device_destroy(my_class,MKDEV(SAMPLE_MAJOR,SAMPLE_MINOR));
+  for(int i=0;i<minor_count;i++){
+    device_destroy(my_class,MKDEV(g_major_num,SAMPLE_MINOR_BASE+i));
+    printk("%s:device_destroy\n",__func__);
+  }
+
   class_destroy(my_class);
-  unregister_chrdev(g_major_num,DEVICE_NAME);
+
+  cdev_del(&my_cdev);
+  unregister_chrdev_region(dev,SAMPLE_MINOR_COUNT);
 }
 
 static struct hello_driver he_drv={
